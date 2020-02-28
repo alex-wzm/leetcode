c588c827a1ccbf2e8187e3b9f8c25e5bf3a10ba6
Alex Wong <alex-wzm@outlook.com>
2020-02-27 16:24:13 -0800

    2nd attempt at 53.py (accepted) - iterative dp only - O(n) time

--- 53_max_subarray.py
+++ 53_max_subarray.py
@@ -2,26 +2,55 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
-        # base case
-        if len(nums) == 1:
-            return nums[0]
         
-        # init output to min_value
+        # init output to min_value and
+        # init accumulator "current_max"
         max_so_far = float('-inf')
+        current_max = 0
         
-        # compare all sub arrays by recursively by storing max of
-        # - max_so_far
-        # - sum of nums (current sub array)
-        # - and sum of nums[i:-1] (pop current sub array)
         for i in range(0, len(nums)):
-            max_so_far = max(
-                max_so_far,
-                sum(nums),
-                self.maxSubArray(nums[i:-1])
-            )
+            # accumulate current_max
+            current_max = current_max + nums[i]
+
+            # store if max sum so far
+            if max_so_far < current_max:
+                max_so_far = current_max
             
+            # reset accumulator if value < 0
+            # i.e. restart counting with next sub-array
+            if current_max < 0:
+                current_max = 0
+                
         return max_so_far
+        """
+        Analysis:
+            - O(n) time
+            - O(n) space
+        """
+        
+
+# class Solution:
+#     def maxSubArray(self, nums: List[int]) -> int:
+
+#         # base case
+#         if len(nums) == 1:
+#             return nums[0]
+        
+#         # init output to min_value
+#         max_so_far = float('-inf')
+        
+#         # compare all sub arrays by recursively by storing max of
+#         # - max_so_far
+#         # - sum of nums (current sub array)
+#         # - and sum of nums[i:-1] (pop current sub array)
+#         for i in range(0, len(nums)):
+#             max_so_far = max(
+#                 max_so_far,
+#                 sum(nums),
+#                 self.maxSubArray(nums[i:-1])
+#             )
+            
+#         return max_so_far
 
 ### TEST CODE ###
 
