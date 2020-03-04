65e6fd049e75d19f0035bd7fb96010d11af8eace
Alex Wong <alex-wzm@outlook.com>
2020-03-03 21:52:12 -0500

    1st attempt at 177.sql (runtime error) - works for given testcase

--- 177_nth_highest_salary.sql
+++ 177_nth_highest_salary.sql
@@ -1,7 +1,13 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
-      # Write your MySQL query statement below.
-      
+      -- Write your MySQL query statement below.
+      SELECT Salary AS "getNthHighestSalary"
+      FROM Employee AS e1
+      WHERE N-1 = (
+          SELECT COUNT(DISTINCT salary)
+          FROM Employee e2
+          WHERE e2.salary > e1.salary
+      )
   );
 END
