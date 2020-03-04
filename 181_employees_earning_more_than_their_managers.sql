8eca8af160605dbf7a14eb9cf4e119d1d61ccf01
Alex Wong <alex-wzm@outlook.com>
2020-03-03 22:10:21 -0500

    1st attempt at 181.sql (accepted) - can be improved: 5th percentile runtime

--- 181_employees_earning_more_than_their_managers.sql
+++ 181_employees_earning_more_than_their_managers.sql
@@ -1,1 +1,8 @@
 -- Write your MySQL query statement below
+SELECT name as "Employee"
+FROM employee as e1
+WHERE salary > (
+    SELECT salary
+    FROM employee as e2
+    WHERE e2.id=e1.managerid
+);
