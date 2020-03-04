64437572a18268a68a1d0d7a40385078e9ae89db
Alex Wong <alex-wzm@outlook.com>
2020-03-03 22:26:37 -0500

    LC solutions vs 1st attempt at 181.sql (accepted)

--- 181_employees_earning_more_than_their_managers.sql
+++ 181_employees_earning_more_than_their_managers.sql
@@ -6,3 +6,20 @@ WHERE salary > (
     FROM employee as e2
     WHERE e2.id=e1.managerid
 );
+
+-- LeetCode Approach 1: Using WHERE clause (Accepted)
+SELECT e1.Name AS 'Employee'
+FROM
+    Employee AS e1,
+    Employee AS e2
+WHERE e1.ManagerId = e2.Id
+    AND e1.Salary > e2.Salary
+;
+
+-- LeetCode Approach 2: Using JOIN clause (Accepted)
+SELECT e1.NAME AS Employee
+FROM Employee AS e1
+JOIN Employee AS e2
+    ON e1.ManagerId = e2.Id
+        AND e1.Salary > e2.Salary
+;
