3fc15560ffa39a8408f81543785043045d3133e2
Alex Wong <alex-wzm@outlook.com>
2020-03-16 09:48:43 -0400

    1st attempt at 1267.py (accepted) - O(n*m) time should be improved upon

--- 1267_count_servers_that_communicate.py
+++ 1267_count_servers_that_communicate.py
@@ -1,2 +1,55 @@
+from typing import List
+
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
+
+        N, M = len(grid), len(grid[0])
+
+        found_servers = [] # store all found servers
+        count_connected_servers = 0 # count how many found servers have at least one connection
+
+        for i in range(N):
+            for j in range(M):
+                if grid[i][j] == 1:
+                    found_servers.append((i,j))
+
+        for server1 in found_servers:
+            for server2 in found_servers:
+                if server1 is not server2:
+                    if server1[0] == server2[0] or server1[1] == server2[1]:
+                        count_connected_servers += 1
+                        break
+
+        return count_connected_servers
+
+
+### TEST CODE ###
+
+s = Solution()
+
+test_cases = [
+    (0, [[1,0],[0,1]]),
+    (3, [[1,0],[1,1]]),
+    (4, [[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]]),
+    (3, [[1,0,0,1,0],[0,0,0,0,0],[0,0,0,1,0]]),
+]
+
+count_passes = 0
+
+for expected_output, test in test_cases:
+    print("="*50)
+    print(f"INPUT: {test}")
+
+    output = s.countServers(test)
+
+    print(f"OUTPUT: {output}")
+    print(f"EXPECTED: {expected_output}\n")
+    
+    if output == expected_output:
+        print("PASS")
+        count_passes += 1
+    else:
+        print("FAIL")
+
+print("="*50)
+print(f"Passed {count_passes}/{len(test_cases)} test cases.\n")
